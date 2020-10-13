85659670ddea284ccd0b9e714a74f86c7449efae
David Bing <davidb.devops@gmail.com>
2020-10-13 13:44:19 -0500

    update CityHTML.js: replace section HTML element with <li>

--- scripts/uk/cities/CityHTML.js
+++ scripts/uk/cities/CityHTML.js
@@ -1,7 +1,5 @@
 export const cityHTML = (cityObj) => {
     return `
-        <section class="city__card">
-            <div class="city__name">${cityObj.name}</div>
-        </section>
+            <li>${cityObj.name}</li>
     `
 }

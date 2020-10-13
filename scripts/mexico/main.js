4043ba2ecc39da67679562d17ef804c27c04909f
David Bing <davidb.devops@gmail.com>
2020-10-13 14:35:14 -0500

    update mexico.html: delete static <li> elements in prep for cityList component

--- scripts/mexico/main.js
+++ scripts/mexico/main.js
@@ -1,7 +1,10 @@
 import { returnMXCities } from './cities/CityDataProvider.js'
-
+import { cityHTML } from './cities/CityHTML.js'
 
 // debug/testing
 const cities = returnMXCities();
 console.log(cities);
+
+const HTML = cityHTML(cities[0]);
+console.log(HTML);
 

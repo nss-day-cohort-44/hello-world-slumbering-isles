b185c5deee3403df6cf2b10aa8a990d0b7fffe5e
Kristen Steele <ksteelephoto@gmail.com>
2020-10-13 15:56:31 -0500

    add import and call for landmarkList to main.js

--- scripts/thailand/main.js
+++ scripts/thailand/main.js
@@ -1,5 +1,8 @@
 import { cityList } from "./cities/CityList.js"
+import { landmarkList } from "./landmarks/LandmarkList.js"
 
 // console.log("I'm Thai Cities main.js!")
 
 cityList()
+
+landmarkList()

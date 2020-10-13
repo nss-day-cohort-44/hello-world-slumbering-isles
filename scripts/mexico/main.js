531b90952e82c8fdb77f1d058ee2f250a6339937
David Bing <davidb.devops@gmail.com>
2020-10-13 14:36:11 -0500

    update main.js: delete debug and test calls/ add cityList call

--- scripts/mexico/main.js
+++ scripts/mexico/main.js
@@ -1,10 +1,13 @@
 import { returnMXCities } from './cities/CityDataProvider.js'
 import { cityHTML } from './cities/CityHTML.js'
+import { cityList } from './cities/CityList.js'
 
 // debug/testing
-const cities = returnMXCities();
-console.log(cities);
+// const cities = returnMXCities();
+// console.log(cities);
 
-const HTML = cityHTML(cities[0]);
-console.log(HTML);
+// const HTML = cityHTML(cities[0]);
+// console.log(HTML);
+
+cityList();
 

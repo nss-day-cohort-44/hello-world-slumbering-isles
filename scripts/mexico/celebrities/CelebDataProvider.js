97aef819c316bc9b45bdb28058caefda34feb35a
David Bing <davidb.devops@gmail.com>
2020-10-14 08:08:24 -0500

    new file: CelebDataProvider.js - implement returnCelebArray and supporting data

---
+++ scripts/mexico/celebrities/CelebDataProvider.js
@@ -0,0 +1,15 @@
+const mxCelebArray = [
+    {
+        name: "Frida Kahlo"
+    },
+    {
+        name: "Salma Hayek"
+    },
+    {
+        name: "Oliver Perez"
+    }
+]
+
+export const returnCelebArray = () => {
+    return mxCelebArray.slice();
+}
